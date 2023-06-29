20f0ca30077d63747811cf5a6aa51a8b90c0f57c
gabbyb21 <gabbailo94@gmail.com>
2023-06-29 16:28:09 -0400

    user can keep using teeth to cut grass

--- game.py
+++ game.py
@@ -1,15 +1,20 @@
 print("Welcome to Landscaper!")
 
 money = 0
-tools = "teeth"
+user_tools = "teeth"
 
 user_input = input("What is your name:")
 print("Hello, " + user_input + "! You are starting a landscaping business, but you only have your teeth to start out with. You can make $1 a day cutting lawns this way.")
 
-action = input("What would you like to do? work/check funds:")
+while True:
+    action = input("What would you like to do? work/check funds/quit: ")
 
-if action == "work":
-    money += 1
-    print("You cut lawns using your teeth and made $1!")
-elif action == "check funds":
-    print(f'You currently have ${money}.')
+    if action == "work":
+        money += 1
+        print("You cut lawns and earned $1!")
+    elif action == "check funds":
+        print(f"You currently have ${money}.")
+    elif action == "quit":
+        break
+    else:
+        print("Invalid. Please try again.")
